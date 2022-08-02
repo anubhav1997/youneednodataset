87ddaf1075c51e1406a927842b9d07f333b9241c
Anubhav Jain <anubhav15129@iiitd.ac.in>
2022-08-02 08:11:10 -0400

    Add files via upload

---
+++ network/AADLayer.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+
+class AADLayer(nn.Module):
+    def __init__(self, c_x, attr_c, c_id, mode=''):
+        super(AADLayer, self).__init__()
+        self.attr_c = attr_c
+        self.c_id = c_id
+        self.c_x = c_x
+        self.mode = mode
+        self.conv1 = nn.Conv2d(attr_c, c_x, kernel_size=1, stride=1, padding=0, bias=True)
+        self.conv2 = nn.Conv2d(attr_c, c_x, kernel_size=1, stride=1, padding=0, bias=True)
+        self.fc1 = nn.Linear(c_id, c_x)
+        self.fc2 = nn.Linear(c_id, c_x)
+        self.norm = nn.InstanceNorm2d(c_x, affine=False)
+
+        self.conv_h = nn.Conv2d(c_x, 1, kernel_size=1, stride=1, padding=0, bias=True)
+
+    def forward(self, h_in, z_attr, z_id):
+        # h_in cxnxn
+        # zid 256x1x1
+        # zattr cxnxn
+        h = self.norm(h_in)
+        gamma_attr = self.conv1(z_attr)
+        beta_attr = self.conv2(z_attr)
+
+        gamma_id = self.fc1(z_id)
+        beta_id = self.fc2(z_id)
+        A = gamma_attr * h + beta_attr
+        gamma_id = gamma_id.reshape(h.shape[0], self.c_x, 1, 1).expand_as(h)
+        beta_id = beta_id.reshape(h.shape[0], self.c_x, 1, 1).expand_as(h)
+        I = gamma_id * h + beta_id
+
+        M = torch.sigmoid(self.conv_h(h))
+        if self.mode =='train_real':
+            out = (torch.ones_like(M) - M) * A + M * I
+        else:
+            out = (torch.ones_like(M).to(device) - M) * A + M * I
+        return out
+
+    
+class AddBlocksSequential(nn.Sequential):
+    def forward(self, *inputs):
+        h, z_attr, z_id = inputs
+        for i, module in enumerate(self._modules.values()):
+            if i%3 == 0 and i > 0:
+                inputs = (inputs, z_attr, z_id)
+            if type(inputs) == tuple:
+                inputs = module(*inputs)
+            else:
+                inputs = module(inputs)
+        return inputs
+        
+        
+class AAD_ResBlk(nn.Module):
+    def __init__(self, cin, cout, c_attr, c_id, num_blocks, mode=''):
+        super(AAD_ResBlk, self).__init__()
+        self.cin = cin
+        self.cout = cout
+        
+        add_blocks = []
+        for i in range(num_blocks):
+            out = cin if i < (num_blocks-1) else cout
+            add_blocks.extend([AADLayer(cin, c_attr, c_id, mode),
+                               nn.ReLU(inplace=True),
+                               nn.Conv2d(cin, out, kernel_size=3, stride=1, padding=1, bias=False)
+                              ])
+        self.add_blocks = AddBlocksSequential(*add_blocks)
+        
+        if cin != cout:
+            last_add_block = [AADLayer(cin, c_attr, c_id, mode),
+                             nn.ReLU(inplace=True), 
+                             nn.Conv2d(cin, cout, kernel_size=3, stride=1, padding=1, bias=False)]
+            self.last_add_block = AddBlocksSequential(*last_add_block)
+            
+
+    def forward(self, h, z_attr, z_id):
+        x =  self.add_blocks(h, z_attr, z_id)
+        if self.cin != self.cout:
+            h = self.last_add_block(h, z_attr, z_id)
+        x = x + h
+        return x
+
+
---
+++ util/norm.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+import numpy as np
+import torch
+class SpecificNorm(nn.Module):
+    def __init__(self, epsilon=1e-8, mode=''):
+        """
+            @notice: avoid in-place ops.
+            https://discuss.pytorch.org/t/encounter-the-runtimeerror-one-of-the-variables-needed-for-gradient-computation-has-been-modified-by-an-inplace-operation/836/3
+        """
+        super(SpecificNorm, self).__init__()
+        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self.mean = np.array([0.485, 0.456, 0.406])
+        self.mean = torch.from_numpy(self.mean).float() #.to(device) #Change this back
+        if mode != 'train_real':
+            self.mean = self.mean.to(device)
+        self.mean = self.mean.view([1, 3, 1, 1])
+
+        self.std = np.array([0.229, 0.224, 0.225])
+        self.std = torch.from_numpy(self.std).float() #.to(device) # change this back
+        if mode != 'train_real':
+            self.std = self.std.to(device)
+        self.std = self.std.view([1, 3, 1, 1])
+
+    def forward(self, x):
+        mean = self.mean.expand([1, 3, x.shape[2], x.shape[3]])
+        std = self.std.expand([1, 3, x.shape[2], x.shape[3]])
+
+        x = (x - mean) / std
+
+        return x
