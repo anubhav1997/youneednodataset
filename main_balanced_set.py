7d36b234d780984834309ddf64f8f7de26bb1d62
Anubhav Jain <anubhav15129@iiitd.ac.in>
2023-12-22 13:30:45 -0500

    Delete main_balanced_set.py

--- main_balanced_set.py
+++
@@ -1,9 +0,0 @@
-from balanced_set import DatasetBalanced, ClassificationModel, get_img_adversarial_optimization, balanced_adv, get_labels, get_labelled_data, train_classification_model, train_SVC, train_GMM
-
-attribute = 'race'
-epochs = 20
-
-
-
-
-
